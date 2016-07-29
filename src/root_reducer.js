87bc0e6ff36ab4cb6760de515c8ae0196026ce22
Joshua Fong <jwfong89@gmail.com>
2016-07-29 16:53:07 -0700

    bugfix(cardlist): fixed minor issues from previous merge conflict

--- src/root_reducer.js
+++ src/root_reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { addFurniture } from './furniture/reducers/add.furniture.reduce';
+// import { addFurniture } from './furniture/reducers/add.furniture.reduce';
 import roomsReducer from './rooms/reducers/rooms.reducer';
 import roomSelectedReducer from './rooms/reducers/roomSelected.reducer';
 
