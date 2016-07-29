716868411f4c24adc21347e79dc6556c8b905cbb
Joshua Fong <jwfong89@gmail.com>
2016-07-29 16:36:02 -0700

    correct merge conflict

--- src/cardList/components/ListItem.js
+++ src/cardList/components/ListItem.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
+import { Row, Col, Card } from 'react-materialize';
 import './cardList.css';
-
-
 
 
 class ListItem extends Component {
   render() {
     return (
-      <div>
-        <li className="list-item">
-
-          <div className="item-description">
-            <h1> {this.props.title} </h1>
-            <p>description</p>
-          </div>
-          
-          <div className="item-media-right">
-            <img/>
-          </div>
-
-        </li>
-      </div>
+      <Row>
+      <Col s={6}>
+        <Card 
+        className='blue-grey darken-1'
+        title={this.props.title}
+        textClassName='white-text'
+        >
+          <p>description</p>
+        </Card>
+      </Col>
+      </Row>
       );
   }
 };
