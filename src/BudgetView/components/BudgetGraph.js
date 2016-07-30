2c0adf880d2e749c1eb729554b75277b2080a0d0
Joshua Fong <jwfong89@gmail.com>
2016-07-30 16:39:32 -0700

    feature(budget) add new component budget
    adding new dudget view component

---
+++ src/BudgetView/components/BudgetGraph.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { }  from 'react-materialize';
+
+class BudgetGraph extends Component {
+  render() {
+    return(
+      <div>this is where a graph should be</div>
+    )
+  }
+} 
+
+export default BudgetView;
