23d12f2fec2e60858ffb31eecee4569a491762c3
Sean Hunter <seanhunter@me.com>
2016-07-30 16:41:46 -0700

    feature(App): Add Footer, add sidebar without functionality
    Reviewed By: Josh

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-// import  from '../rooms/containers/roomsList.container';
 import { Navbar, NavItem, Button, Col, Row} from 'react-materialize';
 
-
 export default class Header extends Component {
+
   render() {
     return (
       <div>
-       <Navbar brand='Nailed-It' right className="indigo darken-1">
-        <NavItem href='#'>Settings</NavItem>
+       <Navbar brand='Nailed-It'  right className="indigo darken-1">
+        <NavItem onClick={function() {this.props.handleToggle}}>Settings</NavItem>
         <NavItem href='#'>Profile</NavItem>
        </Navbar>
      </div>
