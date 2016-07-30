d221731c8132af33fa3ee012259fd7d5ae0261a2
Ben Patalano <ben.patalano@gmail.com>
2016-07-30 14:47:57 -0700

    feature(add room): added a reducer to handle a form
    added a form property to our root reducer to handle forms

--- src/root_reducer.js
+++ src/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 // import { addFurniture } from './furniture/reducers/add.furniture.reduce';
 import roomsReducer from './rooms/reducers/rooms.reducer';
 import selectRoomReducer from './rooms/reducers/selectRoom.reducer.js';
+import { reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
   rooms: roomsReducer,
-  roomSelected: selectRoomReducer
+  roomSelected: selectRoomReducer,
+  form: formReducer
 });
 
 export default rootReducer;
