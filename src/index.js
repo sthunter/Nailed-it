e2b0b916bd38ee6285c2ac84c17e9fbe3b560cc5
Rebero <eber.rene@gmail.com>
2016-07-30 17:15:48 -0700

    feature: set up router to serve App when visiting /

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './app/App';
 import './index.css';
 import reducers from './root_reducer';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
 const store = createStore(reducers);
 
 function render() {
   ReactDOM.render(
-    <App store={store} />,
-    document.getElementById('root')
+    <Provider store={store}>
+      <Router history={browserHistory} routes={routes}/>
+    </Provider>
+    , document.getElementById('root')
   );
 }
 
