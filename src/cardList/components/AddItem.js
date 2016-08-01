bead44619200a3022edfd1ee692e4859b81f83c4
Sean Hunter <seanhunter@me.com>
2016-08-01 16:54:29 -0700

    refactor: modularize form input

--- src/cardList/components/AddItem.js
+++ src/cardList/components/AddItem.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { Card, Button, Modal } from 'react-materialize';
 import './cardList.css';
-import AddRoom from './Modal';
+import AddRoom from './AddRoom';
 
 class AddItem extends Component {
+  state = {
+    form: 'room'
+  }
+
   render() {
-    const store = this.props.store;
+
+
+    // handleSubmit = () => {}
+    const store = this.props.store
+    //this.props.header
+    const header="Add a Room";
+    // const onClick = 
+    const actions = [<Button waves='light' modal='close' flat >Submit</Button>, <Button waves='light' modal='close' flat>Close</Button>]
+    
+    const _form = <AddRoom store={ store } />
     return (
       <Card className="blue-grey lighten-1 center-align">
 
         <Modal
-          header="Modal Header"
+          header={header}
+          actions={actions}
           trigger={
             <Button floating large
               centered="true"
@@ -21,11 +35,14 @@ class AddItem extends Component {
           }
         >
           <div>
-            <AddRoom store={ store } />
+            {(() => {
+              switch (this.state.form) {
+              case "room":   return <AddRoom store={ store }/>;
+              default:      return "Did not work";
+            }
+            })()}
           </div>
         </Modal>
-
-
       </Card>
     );
   }
