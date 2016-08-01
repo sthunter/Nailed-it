651e03b962c44770cc8a5504ccdac8704dc8629d
Joshua Fong <jwfong89@gmail.com>
2016-07-31 22:07:17 -0700

    feature(graph): added react-easy-chart
    testing out react-easy-chart

--- src/BudgetView/components/BudgetGraph.js
+++ src/BudgetView/components/BudgetGraph.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { }  from 'react-materialize';
+import { PieChart } from 'react-easy-chart';
 
 class BudgetGraph extends Component {
   render() {
     return(
-      <div>this is where a graph should be</div>
+      <div>
+        <PieChart
+          data={[
+            {key: 'A', value: 100, color: '#aaac84'},
+            {key: 'B', value: 200, color: '#dce7c5'},
+            {key: 'C', value: 50, color: '#e3a51a'}
+          ]}
+        />
+      </div>
     )
   }
 } 
