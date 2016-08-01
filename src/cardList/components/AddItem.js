b2e43e0cab57d99767b0923b33a93db58d005614
Nick Barry <niko.barry@gmail.com>
2016-08-01 10:15:25 -0700

    style: Spacing/indentation changes

--- src/cardList/components/AddItem.js
+++ src/cardList/components/AddItem.js
@@ -7,27 +7,27 @@ class AddItem extends Component {
   render() {
     const store = this.props.store;
     return (
-                <Card className="blue-grey lighten-1 center-align">
-              
-            <Modal
-              header="Modal Header"
-              trigger={
-                <Button floating large
-                  centered="true"
-                  className="grey"
-                  waves="light"
-                  icon="add"
-                />
-              }
-            >
-              <div>
-                <AddRoom store={ store } />
-              </div>
-            </Modal>
+      <Card className="blue-grey lighten-1 center-align">
+
+        <Modal
+          header="Modal Header"
+          trigger={
+            <Button floating large
+              centered="true"
+              className="grey"
+              waves="light"
+              icon="add"
+            />
+          }
+        >
+          <div>
+            <AddRoom store={ store } />
+          </div>
+        </Modal>
 
 
-            </Card>
-      );
+      </Card>
+    );
   }
 };
 
