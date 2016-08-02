b38c25da0f6e2f6b3e26073b776dcc549801b4b5
Ben Patalano <ben.patalano@gmail.com>
2016-08-01 17:39:21 -0700

    feat(budget view): used actions to call on firebase database
    -created action, reducer, and container for budget view
    -modified root_reducer to keep track of budget
    -successfully called firebase api to get info from database

--- src/root_reducer.js
+++ src/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import roomsReducer from './rooms/reducers/rooms.reducer';
 import selectRoomReducer from './rooms/reducers/selectRoom.reducer.js';
 import { reducer as formReducer} from 'redux-form';
+import budgetReducer from './BudgetView/reducers/BudgetView.reducer';
 
 const rootReducer = combineReducers({
   rooms: roomsReducer,
   roomSelected: selectRoomReducer,
-  form: formReducer
+  form: formReducer,
+  budget: budgetReducer,
 });
 
 export default rootReducer;
