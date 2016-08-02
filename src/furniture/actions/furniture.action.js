25a6aef7fd54a6a0705a0c8b6e11b6d5650afff1
Nick Barry <niko.barry@gmail.com>
2016-08-02 12:29:03 -0700

    feat (frontend): Add actions and reducers for deleting and updating furniture
    This adds actions and reducers that would allow a user to delete or update furniture. It isn't yet hooked into the UI, though, so the actions and reducers aren't actually being called anywhere.
    
    Reviewed by Sean

--- src/furniture/actions/furniture.action.js
+++ src/furniture/actions/furniture.action.js
@@ -1,12 +1,30 @@
 export const ADD_FURNITURE = 'ADD_FURNITURE';
+export const DELETE_FURNITURE = 'DELETE_FURNITURE';
+export const UPDATE_FURNITURE = 'UPDATE_FURNITURE';
 
-/*
- * action creators
- */
-  export function addFurniture(furniture, room) {
+// NOTE: Many corresponding reducers are in rooms.reducer, not in the furniture directory
+
+export function addFurniture(furnitureName, roomName) {
   return {
     type: ADD_FURNITURE,
-    furniture,
-    room
-  }; // NOTE: The reducer is in rooms.reducer, not in the furniture directory
+    furnitureName,
+    roomName
+  };
+}
+
+export function deleteFurniture(furnitureName, roomName) {
+  return {
+    type: DELETE_FURNITURE,
+    furnitureName,
+    roomName
+  };
+}
+
+export function updateFurniture(furnitureName, roomName, newProps) {
+  return {
+    type: UPDATE_FURNITURE,
+    roomName,
+    furnitureName,
+    newProps
+  }
 }
