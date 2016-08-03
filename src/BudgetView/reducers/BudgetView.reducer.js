dc21fec54e025c24966135051ca1df56336ebe5e
Ben Patalano <ben.patalano@gmail.com>
2016-08-03 15:35:47 -0700

    feat(set budget): added an update budget case to the budget reducer
    -reducer now handles case where UPDATE_BUDGET action is submitted
    
    Reviewed by: Rene

--- src/BudgetView/reducers/BudgetView.reducer.js
+++ src/BudgetView/reducers/BudgetView.reducer.js
@@ -1,10 +1,11 @@
-import { GET_BUDGET } from '../actions/budgetView.action';
+import { GET_BUDGET, UPDATE_BUDGET } from '../actions/budgetView.action';
 
 const budgetReducer = (state = null, action) => {
   switch (action.type) {
     case GET_BUDGET:
-      
       return action.payload.val();
+    case UPDATE_BUDGET:
+      return action.newBudget;
     default:
       return state;
   }
