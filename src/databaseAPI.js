e5dcc2f43f5495ee5121fe2eb29e932392ac7e12
Ben Patalano <ben.patalano@gmail.com>
2016-08-03 11:44:22 -0700

    refactor(database API): commented out unused functions

--- src/databaseAPI.js
+++ src/databaseAPI.js
@@ -29,13 +29,13 @@ export function getRooms(cb) {
   });
 };
 
-export function getFurniture(rooms) {
-  for (var room in rooms) {
-    database.ref('rooms/' + room + '/furniture').once('value', function(snapshot) {
-      return snapshot;
-    })
-  }
-};
+// export function getFurniture(rooms) {
+//   for (var room in rooms) {
+//     database.ref('rooms/' + room + '/furniture').once('value', function(snapshot) {
+//       return snapshot;
+//     })
+//   }
+// };
 
 export function getBudget() {
   return database.ref('budget').once('value')
@@ -48,24 +48,24 @@ export function updateBudget(budget) {
   database.ref('budget').set(budget);
 };
 
-export function updateFurniture(rooms, furniture) {
-  for (var room in rooms) {
-    for (var pieceOfFurniture in furniture) {
-      database.ref('rooms/' + room + '/furniture/' + pieceOfFurniture).set(furniture[pieceOfFurniture]);
-    }
-  }
-};
+// export function updateFurniture(rooms, furniture) {
+//   for (var room in rooms) {
+//     for (var pieceOfFurniture in furniture) {
+//       database.ref('rooms/' + room + '/furniture/' + pieceOfFurniture).set(furniture[pieceOfFurniture]);
+//     }
+//   }
+// };
 
-export function removeRoom(room) {
-  for (var room in rooms) {
-    database.ref('rooms/' + room).remove();
-  }
-};
+// export function removeRoom(room) {
+//   for (var room in rooms) {
+//     database.ref('rooms/' + room).remove();
+//   }
+// };
 
-export function removeFurniture(room, furniture) {
-  for (var room in rooms) {
-    for (var pieceOfFurniture in furniture) {
-      database.ref('rooms/' + room + '/furniture/' + pieceOfFurniture).remove();
-    }
-  }
-};
+// export function removeFurniture(room, furniture) {
+//   for (var room in rooms) {
+//     for (var pieceOfFurniture in furniture) {
+//       database.ref('rooms/' + room + '/furniture/' + pieceOfFurniture).remove();
+//     }
+//   }
+// };
