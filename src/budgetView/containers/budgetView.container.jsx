b6cc5a742a8c32bb01b989d382af4c62cc322302
Ben Patalano <ben.patalano@gmail.com>
2016-08-03 15:38:50 -0700

    feat(update budget): turned budget view into a redux form
    -change budget view to redux form to allow users to update budget
    -form properly interacts with redux state and firebase database through api
    
    Reviewed by: Rene

--- src/budgetView/containers/budgetView.container.jsx
+++ src/budgetView/containers/budgetView.container.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import {Button, Input} from 'react-materialize';
 import BudgetTable from '../components/BudgetTable';
 import BudgetGraph from '../components/BudgetGraph';
-import { getBudget } from '../actions/budgetView.action';
+import { getBudget, updateBudget } from '../actions/budgetView.action';
+import { reduxForm } from 'redux-form';
 
 
 class BudgetView extends Component {
@@ -12,24 +14,33 @@ class BudgetView extends Component {
   }
   
   render() {
+    const { fields: { newBudget }, handleSubmit } = this.props;
     return(
       <div>
         <BudgetTable budget={this.props.budget} rooms={this.props.rooms}/>
         <BudgetGraph rooms={this.props.rooms}/>
+      
+        <form onSubmit={ handleSubmit(this.props.updateBudget) }>
+          <Input type="number" placeholder="Budget" s={12} label="Budget" { ...newBudget } />
+          <Button type="submit">Update</Button>
+        </form>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    budget: state.budget
-  };
-}
+// function mapStateToProps(state) {
+//   return {
+//     budget: state.budget
+//   };
+// }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({getBudget}, dispatch);
-}
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({getBudget}, dispatch);
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(BudgetView);
+export default reduxForm({
+  form: 'BudgetView',
+  fields: ['newBudget']
+}, state => ({ budget: state.budget}),{ getBudget, updateBudget })(BudgetView);
 
