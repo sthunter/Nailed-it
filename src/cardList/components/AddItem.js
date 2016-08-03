786bd091ca32de55ff01ce3ed134576497f6fcdd
Nick Barry <niko.barry@gmail.com>
2016-08-02 19:04:47 -0700

    chore: Dealing with git's weird rebase issues

--- src/cardList/components/AddItem.js
+++ src/cardList/components/AddItem.js
@@ -6,37 +6,15 @@ import AddFurniture from './AddFurniture';
 import FurnitureDetail from '../../furniture/components/FurnitureDetail';
 
 class AddItem extends Component {
-
   render() {
-    //const header = this.props.vw === 'room' ? 'Add a room' : 'Add a piece of furniture';
-    //const actions = [<Button waves='light' modal='close' flat >Submit</Button>, <Button waves='light' modal='close' flat>Close</Button>];
-
     return (
       <div>
-        {this.props.view === 'room' ? <AddRoom /> : <AddFurniture />}
+        {this.props.view === 'room' ?
+          <AddRoom /> :
+          <AddFurniture />
+        }
       </div>
     );
-    //return (
-    //  <Card className="grey lighten-4 center-align">
-    //
-    //    <Modal
-    //      header={header}
-    //      actions={actions}
-    //      trigger={
-    //        <Button floating large
-    //          centered="true"
-    //          className="grey lighten-1"
-    //          waves="light"
-    //          icon="add"
-    //        />
-    //      }
-    //    >
-    //      <div>
-    //        <AddRoom />
-    //      </div>
-    //    </Modal>
-    //  </Card>
-    //);
   }
 };
 
