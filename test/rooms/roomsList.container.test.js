708901ae6001af0387be64eeed79f749158f3f03
Nick Barry <niko.barry@gmail.com>
2016-08-04 14:55:20 -0700

    test: Set up testing infrastructure
     - Create a dummy test file that runs a few passing tests
     - Set up .babelrc file
     - Update package.json: Add packages, and add testing scripts

---
+++ test/rooms/roomsList.container.test.js
@@ -0,0 +1,27 @@
+import { renderComponent, expect } from '../testHelper';
+import RoomsList from '../../src/rooms/containers/roomsList.container.jsx';
+
+describe('RoomsList', () => {
+  let component;
+
+  //beforeEach(() => {
+  //  component = renderComponent(RoomsList);
+  //});
+
+  it('shows a comment box', () => {
+    expect(true).to.be.true;
+    expect(true).to.be.true;
+    //expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('shows a comment box2', () => {
+    expect(true).to.be.true;
+    expect(true).to.be.true;
+    //expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('shows a comment list', () => {
+    expect(true).to.be.true;
+    //expect(component.find('.comment-list')).to.exist;
+  });
+});
--- test/testHelper.jsx
+++ test/testHelper.jsx
@@ -9,13 +9,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/root.reducer';
 
+// Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
 const $ = _$(window);
 
+// Set up chai-jquery
 chaiJquery(chai, chai.util, $);
 
+// build 'renderComponent' helper that should render a given react class
 function renderComponent(ComponentClass, props = {}, state = {}) {
   const componentInstance =  TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
@@ -26,6 +29,7 @@ function renderComponent(ComponentClass, props = {}, state = {}) {
   return $(ReactDOM.findDOMNode(componentInstance));
 }
 
+// Build helper for simulating events
 $.fn.simulate = function (eventName, value) {
   if (value) {
     this.val(value);
