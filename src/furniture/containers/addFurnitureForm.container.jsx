36075f8a0e2f9756d327946bc4792c59cf833b28
Sean Hunter <seanhunter@me.com>
2016-08-04 14:54:32 -0700

    Style: Add Datepicker on add furniture form

--- src/furniture/containers/addFurnitureForm.container.jsx
+++ src/furniture/containers/addFurnitureForm.container.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Input, Row, Col } from 'react-materialize';
 import { reduxForm } from 'redux-form';
 import { addFurniture } from '../actions/furniture.action.js';
+import { DatePicker } from 'material-ui/DatePicker'
 
 class AddFurnitureForm extends Component {
   render() {
@@ -9,6 +10,7 @@ class AddFurnitureForm extends Component {
       itemName, price, description, url, deliveryDate,
       }, handleSubmit, } = this.props;
 
+        
     return (
       <Col s={12} l={6}>
         <form onSubmit={ handleSubmit(this.props.addFurniture.bind(null, this.props.roomSelected)) }>
@@ -21,7 +23,9 @@ class AddFurnitureForm extends Component {
             <Input s={6} placeholder='URL'{ ...url } />
           </Row>
           <Row>
-            <Input s={6} placeholder='Delivery Date' label='Date' { ...deliveryDate } />
+            <Col s={6}>
+              <input type='date' className='datepicker'{...deliveryDate} />
+            </Col>
           </Row>
           <Button type="submit">Submit</Button>
         </form>
@@ -34,3 +38,5 @@ export default reduxForm({
   form: 'AddFurnitureForm',
   fields: ['itemName', 'price', 'description', 'url', 'deliveryDate', 'roomSelected'],
 }, state => ({ roomSelected: state.roomSelected }), { addFurniture })(AddFurnitureForm);
+              
+
