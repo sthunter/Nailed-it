a87718afc95baea89575f1ec15c68ede4edae1d1
Joshua Fong <jwfong89@gmail.com>
2016-08-04 10:49:57 -0700

    feature(budgetGraph): data being shown in pie chart
    got pie chart to show according to room

--- src/BudgetView/components/budgetGraph.jsx
+++ src/BudgetView/components/budgetGraph.jsx
@@ -3,17 +3,79 @@ import { }  from 'react-materialize';
 import { PieChart } from 'react-easy-chart';
 
 class BudgetGraph extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.generateData = this.generateData.bind(this);
+  }
+
+  // test() {
+  //   return [
+  //           {key: 'A', value: 100, color: '#aaac84'},
+  //           {key: 'B', value: 200, color: '#dce7c5'},
+  //           {key: 'C', value: 50, color: '#e3a51a'}
+  //         ]
+  // }
+
+generateData(rooms) {
+  console.log("rooms: ", rooms);
+  let arr = [];
+  // const roomsList = Object.keys(rooms);
+  // console.log('keys: ', Object.keys(rooms));
+  const roomsList = ['bathroom', 'bedroom', 'dining room', 'living room', 'kitchen'];
+  // const roomsList = [1,2,3,4,5]
   
 
+  var getRandomColor = function() {
+      var letters = '0123456789abcdef';
+      var color = '#';
+      for (var i = 0; i < 6; i++ ) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+   var calculateBudget = function(roomname){
+      let sum = 0;
+
+      if(rooms[roomname]) {
+        let list = rooms[roomname].furniture;
+        for(var key in list) {
+          if(key){
+            sum += Number(list[key].price);
+          }
+        }
+      }
+      return sum;
+    }
+
+  roomsList.map(function(room, id){
+    // for(let room in rooms){
+      // if(rooms.hasOwnProperty(room)){
+        var obj = {};
+        obj["key"] = room;
+        obj["value"] = calculateBudget(room);
+        obj["color"] = getRandomColor();
+        console.log(room)
+        arr.push(obj)
+   //    }
+   // }
+  });
+      console.log(arr)
+    return arr;
+}
+
   render() {
+    const genData = this.generateData(this.props.rooms)
+    console.log(genData)
+    // const test = this.test();
+    // console.log('test invoked', test)
     return(
       <div>
         <PieChart
-          data={[
-            {key: 'A', value: 100, color: '#aaac84'},
-            {key: 'B', value: 200, color: '#dce7c5'},
-            {key: 'C', value: 50, color: '#e3a51a'}
-          ]}
+          labels
+          data={ genData }
         />
       </div>
     )
