acd9ec1a87090baa740105605dfdcab77850f8d8
Rebero <eber.rene@gmail.com>
2016-08-04 21:21:42 -0700

    feature: return proper state on uploading new photos

--- src/rooms/reducers/addRoomPhoto.reducer.js
+++ src/rooms/reducers/addRoomPhoto.reducer.js
@@ -3,7 +3,7 @@ import { ADD_PHOTO } from '../actions/rooms.action';
 const addPhoto = (state = {}, action) => {
   switch (action.type) {
     case ADD_PHOTO:
-      return {};
+      return action.url;
     default:
       return state;
   }
