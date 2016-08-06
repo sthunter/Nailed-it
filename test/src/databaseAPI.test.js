b8009136c50cd197ceca21776602e15b44210429
Ben Patalano <ben.patalano@gmail.com>
2016-08-06 16:34:07 -0700

    refactor(database test): gave up on testing firebase but left file

--- test/src/databaseAPI.test.js
+++ test/src/databaseAPI.test.js
@@ -1,6 +1,16 @@
 import getRooms from '../../src/databaseAPI';
 import { expect } from '../testHelper.jsx';
 
-describe('databaseAPI', () => {
-
-});
+// describe('databaseAPI', () => {
+  // describe('getRooms', () =>{
+    // it('returns a promise that resolves to an object with a color array in it', () => {
+    //   let get = getRooms();
+    //   expect(get).to.be.a('promise');
+    //   get.then((snapshot) => {
+    //     let result = snapshot.val();
+    //     expect(result).to.have.deep.property('bathroom.colors')
+    //       .to.equal("#45425B;#575D55;#6D7D47;#96B13A;#BCC12F")
+    //   })
+    // })
+//   })
+// });
