1df647b7f916f7fb1980f68770de43891a80b497
Sean Hunter <seanhunter@me.com>
2016-08-08 19:22:00 -0700

    style(wip): color swatches on roomslist

--- src/rooms/components/listItem.component.jsx
+++ src/rooms/components/listItem.component.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import { removeRoom } from '../actions/rooms.action';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { SwatchPicker } from 'react-color'
 
 class ListItem extends Component {
   handleClick(title) {
@@ -16,25 +17,67 @@ class ListItem extends Component {
     Materialize.toast(title + ' removed', 4000)
   }
 
+  state = {
+    displayColorPicker: false,
+  };
+
+  handleColorClose = () => {
+    this.setState({ displayColorPicker: false })
+  };
+
+  handleColorClick () {
+    console.log('got here')
+    this.setState({ displayColorPicker: !this.state.displayColorPicker })
+  };
+
+
   render() {
+<<<<<<< 7e850edf41728f60e8790f56c49dbe2f1b6d8dc3
+||||||| merged common ancestors
+
+=======
+
+
+    const popover = {
+      position: 'absolute',
+      zIndex: '2',
+    }
+    const cover = {
+      position: 'fixed',
+      top: '0',
+      right: '0',
+      bottom: '0',
+      left: '0',
+    }
+>>>>>>> style(wip): color swatches on roomslist
     const title = this.props.title;
+    
     return (
+<<<<<<< 7e850edf41728f60e8790f56c49dbe2f1b6d8dc3
       <Link to={ 'furniture/' + this.props.title }>
+||||||| merged common ancestors
+      <Link to={ 'furniture' }>
+=======
+>>>>>>> style(wip): color swatches on roomslist
         <CardPanel
           onClick={ () => this.handleClick(title) }
           className='grey lighten-2 ListItem card-panel hoverable'
-          textClassName='black-text'
         >
-          <div className='card-title'>
-            <span>{title}</span>
+          <div>
+            <span><Link className="card-title" to={ 'furniture' }>{title}</Link></span>
           </div>
-          <div className='card-controls'>
-            <i className="material-icons md-dark">add_a_photo</i>
-            <i className="material-icons md-dark">create</i>
-            <i className="material-icons md-dark" onClick={() => {this.removeRoomCall(title)} }>delete_sweep</i>
+          <div className=''>
+            <i className="card-controls material-icons md-dark">add_a_photo</i>
+            <i className="card-controls material-icons md-dark">create</i>
+            <i className="card-controls material-icons md-dark" onClick={() => {this.removeRoomCall(title)}}>delete_sweep</i>
+            <i className="card-controls material-icons md-dark" onClick={() => {this.handleColorClick}}>format_paint</i>
+            { this.state.displayColorPicker ? <div style={ popover }>
+              <div style={ cover } onClick={ this.handleColorClose }/>
+              <SwatchPicker />
+              </div> : null 
+            }
           </div>
         </CardPanel>
-      </Link>
       );
   }
 }
