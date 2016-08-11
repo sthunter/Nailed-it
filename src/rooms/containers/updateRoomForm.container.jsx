08c886efcacd6b3d05151491a05f105dd7aec35e
Ben Patalano <ben.patalano@gmail.com>
2016-08-10 18:46:05 -0700

    feat(update room form): created file for update room form

---
+++ src/rooms/containers/updateRoomForm.container.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Button, Input, Col, Row } from 'react-materialize';
+import { reduxForm } from 'redux-form';
+import { updateRoom } from '../actions/rooms.action.js';
+
+class UpdateAddRoomForm extends Component {
+  render() {
+    const { fields: { roomName, size, notes }, handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={ handleSubmit(this.props.addRoom) } >
+        <Col s={12}>
+          <Row>
+            <Input type="text" placeholder="Room Name" s={6} label="Room Name" { ...roomName } />
+            <Input type="text" placeholder="Size" s={6} label="Size" { ...size } />
+          </Row>
+          <Row>
+            <div class="input-field col s12">
+              <label for="textarea1">Notes</label>
+              <textarea id="textarea1" className="materialize-textarea" placeholder="Notes" { ...notes }></textarea>
+            </div>
+          </Row>
+        </Col>
+        <Button type="submit">Submit</Button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'UpdateAddRoomForm',
+  fields: ['roomName', 'size', 'notes'],
+}, null, { addRoom })(UpdateAddRoomForm);
