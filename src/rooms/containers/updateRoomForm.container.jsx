ead860b7f373262e71aab053c333d376a6579494
Ben Patalano <ben.patalano@gmail.com>
2016-08-10 19:35:10 -0700

    feat(update room form): continued work on update room form
    -not finished or tested

--- src/rooms/containers/updateRoomForm.container.jsx
+++ src/rooms/containers/updateRoomForm.container.jsx
@@ -3,21 +3,22 @@ import { Button, Input, Col, Row } from 'react-materialize';
 import { reduxForm } from 'redux-form';
 import { updateRoom } from '../actions/rooms.action.js';
 
-class UpdateAddRoomForm extends Component {
+class UpdateRoomForm extends Component {
   render() {
-    const { fields: { roomName, size, notes }, handleSubmit } = this.props;
+    const { fields: { roomName, size, notes }, handleSubmit, roomSelected, rooms, updateRoom } = this.props;
+    const details = rooms[roomSelected];
 
     return (
-      <form onSubmit={ handleSubmit(this.props.addRoom) } >
+      <form onSubmit={ handleSubmit(updateRoom.bind(null, roomSelected, details)) } >
         <Col s={12}>
           <Row>
-            <Input type="text" placeholder="Room Name" s={6} label="Room Name" { ...roomName } />
-            <Input type="text" placeholder="Size" s={6} label="Size" { ...size } />
+            <Input type="text" placeholder="Room Name" s={6} label="Room Name" defaultValue={ roomSelected } { ...roomName } />
+            <Input type="text" placeholder="Size" s={6} label="Size" defaultValue={ details.size } { ...size } />
           </Row>
           <Row>
             <div class="input-field col s12">
               <label for="textarea1">Notes</label>
-              <textarea id="textarea1" className="materialize-textarea" placeholder="Notes" { ...notes }></textarea>
+              <textarea id="textarea1" className="materialize-textarea" placeholder="Notes" defaultValue={ details.notes } { ...notes }></textarea>
             </div>
           </Row>
         </Col>
@@ -28,6 +29,6 @@ class UpdateAddRoomForm extends Component {
 }
 
 export default reduxForm({
-  form: 'UpdateAddRoomForm',
+  form: 'UpdateRoomForm',
   fields: ['roomName', 'size', 'notes'],
-}, null, { addRoom })(UpdateAddRoomForm);
+}, state => ({ roomSelected: state.roomSelected, rooms: state.rooms }), { updateRoom })(UpdateRoomForm);
