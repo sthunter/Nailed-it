ef49ec64c8f828df458ecb8e3376ddfbd992dd7f
Ben Patalano <ben.patalano@gmail.com>
2016-08-11 17:25:11 -0700

    refactor(budget graph): pulled out some information to make it simple

--- src/BudgetView/components/budgetGraph.jsx
+++ src/BudgetView/components/budgetGraph.jsx
@@ -1,101 +1,46 @@
 import React, { Component } from 'react';
-import { PieChart } from 'react-easy-chart';
+
 import { Table, Col, Row  } from 'react-materialize';
+import budgetHelper from '../budgetHelperFunctions';
+import PieChartHolder from './pieChartHolder.component.jsx';
 
 
 class BudgetGraph extends Component {
 
+
   constructor(props) {
     super(props);
-    this.generateData = this.generateData.bind(this);
-    this.calculateBudget = this.calculateBudget.bind(this);
-  
-    }
-
-  calculateBudget(roomname) {
-    let sum = 0;
-
-    if(this.props.rooms[roomname]) {
-      let list = this.props.rooms[roomname].furniture;
-      if(typeof(list) === 'object') {
-        for(var key in list) {
-          if(key){
-            sum += Number(list[key].price);
-          }
-        }
-      }
-    }
-    return sum;
-  }
-
-  getRandomColor() {
-    var letters = '0123456789abcdef';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  toTitleCase(str) {
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
-
-  generateData(rooms) {
+    //budgetHelper.generateData = budgetHelper.generateData.bind(this);
+    //budgetHelper.calculateBudget = budgetHelper.calculateBudget.bind(this);
     
-    let arr = []
-    let totalCost = 0;
-    let currentBudget = this.props.budget;
-    const roomsList = Object.keys(rooms);
-    const calculateBudget = this.calculateBudget;
-    const getRandomColor = this.getRandomColor;
-    const toTitleCase = this.toTitleCase;
-
-    roomsList.forEach(function(room){
-      let calc = calculateBudget(room);
-      
-     
-      var obj = {};
-      obj["key"] = calc > 0 ? toTitleCase(room) : ' ';
-      obj["value"] = calc;
-      obj["color"] = rooms[room].color.hex;
-      arr.push(obj)
-      totalCost += calc;
-    });
-    
-    let remainingBudget = currentBudget - totalCost;
-    
-    let costObj = {};
-    costObj["key"] = remainingBudget >= 0 ? "Remaining Budget" : "";
-    costObj["value"] = remainingBudget > 0 ? remainingBudget : 0;
-    costObj["color"] = "#424242";
-    arr.push(costObj);
-
-    return arr;
+  }
+  componentWillReceiveProps(nextProps) {
+    if(Object.keys(this.props.rooms).length !== 0) {
+      var genData = budgetHelper.generateData(this.props.rooms);
+    }
   }
 
   render() {
     var genData;
-    const CurrentRooms = this.props.rooms;
-    const BudgetRoom = Object.keys(CurrentRooms);
-    const currentBudget = this.props.budget;
-    const calcBudget = this.calculateBudget;
-    const toTitleCase = this.toTitleCase;
-
-    if(Object.keys(this.props.rooms).length !== 0) {
-      genData = this.generateData(CurrentRooms)
+    
+    const { rooms, budget } = this.props;
+    const roomNames = Object.keys(rooms);
+    const calcBudget = budgetHelper.calculateBudget;
+    const toTitleCase = budgetHelper.toTitleCase;
+    if(Object.keys(rooms).length !== 0) {
+      console.log(rooms[roomNames[0]].color.hex);
+      genData = budgetHelper.generateData(rooms);
     }
-    var that = this;
 
     return(
-      Object.keys(that.props.rooms).length !== 0 ? 
+      Object.keys(rooms).length !== 0 ? 
         <div>
           <Row>
             <Col>
-              <h5><b>Total Budget:</b> { currentBudget || 'no budget' }</h5>
+              <h5><b>Total Budget:</b> { budget || 'no budget' }</h5>
               <h5><b>Total Cost:</b> { this.totalCost || 'no costs' }</h5>
               <h5><b>Budget Remaining:</b> { 
-                currentBudget - this.totalCost > 0 ? currentBudget - this.totalCost : <span style={{'color': 'red'}}>{currentBudget - this.totalCost}</span>
+                budget - this.totalCost > 0 ? budget - this.totalCost : <span style={{'color': 'red'}}>{budget - this.totalCost}</span>
               }</h5>
             </Col>
           </Row>
@@ -107,32 +52,23 @@ class BudgetGraph extends Component {
             </tr>
           </thead>
           <tbody>
-            {BudgetRoom.map((roomName, id)=> {
+            {roomNames.map((roomName, id)=> {
               if (id === 0) {
                 this.totalCost = 0;
               }
-              let roomCost = calcBudget(roomName);
+              let roomCost = calcBudget(roomName, rooms);
               this.totalCost += roomCost;
               return (
                 <tr 
                 key={id}>
-                  <th>{ toTitleCase(roomName) }</th>
-                  <th>{ calcBudget(roomName) } </th>
+                  <th>{ budgetHelper.toTitleCase(roomName) }</th>
+                  <th>{ roomCost } </th>
                 </tr>
                 )
             })}
           </tbody>
         </Table>  
-
-        <PieChart
-            labels
-            size={ 330 }
-            data={ genData }
-            clickHandler={(selection) => {
-              let data = this.props.onClick(selection.data);
-            }}
-
-          />
+        <PieChartHolder genData ={genData} />
         </div> : <div>Loading...</div>
     )
   }
