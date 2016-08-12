1958993c111ef74244f27d7467ada350e4651365
Ben Patalano <ben.patalano@gmail.com>
2016-08-12 11:45:42 -0700

    refactor(list item): refactored into a component again
    -removed actions and state to turn listItem back into a component

--- src/rooms/components/listItem.component.jsx
+++ src/rooms/components/listItem.component.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { CardPanel, Button, Row, Col, MediaBox } from 'react-materialize';
 import { Link, browserHistory } from 'react-router';
-import { removeRoom, addPhoto, selectRoom } from '../actions/rooms.action';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import Dropzone from 'react-dropzone';
-import ColorInput from './colorPicker.component';
+import Toolbar from './toolbar.component'
+
+
 
 
 class ListItem extends Component {
@@ -15,23 +13,6 @@ class ListItem extends Component {
       this.props.clickHandler(title);
     }
   }
-  removeRoomCall(title) {
-    this.props.removeRoom(title);
-    Materialize.toast(title + ' removed', 4000)
-  }
-
-  state = {
-    displayColorPicker: false,
-  }
-
-  handleColorClick () {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker })
-  }
-
-  onDrop(files, title) {
-    this.props.addPhoto(files, title);
-  }
-
   render() {
     const popover = {
       position: 'absolute',
@@ -68,32 +49,10 @@ class ListItem extends Component {
               </Col>
             </Row>
           </Link>
-            <Row>
-              <Col l={6}>
-                  <div className='card-control' hoverable>
-                    <Dropzone style={{'width': '24px', 'height': '24px', 'border': '0px'}} onDrop={files => this.onDrop(files, title)}>
-                      <i className="card-controls material-icons md-dark">add_a_photo</i>
-                    </Dropzone>
-                  </div>
-                  <div className='card-control' hoverable><i className="card-controls material-icons md-dark">create</i></div>
-                  <div className='card-control' hoverable><i className="card-controls material-icons md-dark" onClick={() => {this.removeRoomCall(title)}}>delete_sweep</i></div>
-                  <Link to={ 'designer/' + title }> 
-                    <div className='card-control' hoverable><i className="card-controls material-icons md-dark" >gesture</i></div>
-                  </Link>
-                  <ColorInput />
-              </Col>
-            </Row>
+          <Toolbar title={ title } />
         </CardPanel>
       );
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return   bindActionCreators({ removeRoom, addPhoto }, dispatch);
-}
-
-function mapStateToProps({ rooms, roomSelected, shared, route }) {
-  return { rooms, roomSelected, shared, route };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ListItem);
+export default ListItem;
