ea9e8afbfd7bb75f0cd48f8f0db6114c3ae9e936
Ben Patalano <ben.patalano@gmail.com>
2016-08-11 17:22:23 -0700

    refactor(budget graph): pulled the pie chart out to its own component

---
+++ src/BudgetView/components/pieChartHolder.component.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { PieChart } from 'react-easy-chart';
+
+
+class PieChartHolder extends Component {
+  state = {
+    genData: this.props.genData
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({genData: nextProps.genData})
+  }
+
+  
+
+  render() {
+    const genData = this.state.genData;
+    console.log(this.state.genData);
+    return(
+      <PieChart
+          labels
+          size={ 330 }
+          data={ genData }
+          clickHandler={(selection) => {
+            let data = this.props.onClick(selection.data);
+          }}
+
+        />
+    )
+  }
+};
+
+export default PieChartHolder
