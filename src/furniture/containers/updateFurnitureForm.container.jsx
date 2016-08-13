2c3e7aa8e6446cff8410c2be775fa48ad9ebaa13
Nick Barry <niko.barry@gmail.com>
2016-08-13 16:30:55 -0700

    feat(ui): Convert all furniture details to inputs
    Now all the furniture details are displayed as inputs with initial values. There is a form-submit action, but it doesn't work yet.

--- src/furniture/containers/updateFurnitureForm.container.jsx
+++ src/furniture/containers/updateFurnitureForm.container.jsx
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
-import { Table, Input } from 'react-materialize';
+import { UPDATE_FURNITURE, updateFurniture } from '../actions/furniture.action';
+//import { Table, Input } from 'react-materialize';
 
 class UpdateFurnitureForm extends Component {
   render() {
-    const { fields: { itemName } } = this.props;
-    const name = this.props.name;
+    const { fields: { itemName, price, deliveryDate, size, description }, handleSubmit } = this.props;
+    console.log(itemName);
+    // todo: Need to pass action into handleSubmit
 
     return (
-      <Table className="furniture-detail">
-        <tbody>
-          <tr>
-            <th>Name</th><td><Input type="text" { ...itemName } defaultValue={ name } /></td>
-            <th>Price</th><td>{ this.props.details.price }</td>
-          </tr>
-          <tr>
-            <th>Delivery</th><td>{ this.props.details.deliveryDate }</td>
-            <th>Size</th><td>{ this.props.details.size }</td>
-          </tr>
-          <tr>
-            <th>Notes</th><td colSpan="3">{ this.props.details.description }</td>
-          </tr>
-        </tbody>
-      </Table>
+      <form onSubmit={ handleSubmit(updateFurniture.bind(null, this.props.name, this.props.roomSelected)) }>
+        <table className="furniture-detail">
+          <tbody>
+            <tr>
+              <th>Name</th><td><input type="text" { ...itemName }  /></td>
+              <th>Price</th><td><input type="text" { ...price }  /></td>
+            </tr>
+            <tr>
+              <th>Delivery</th><td><input type="text" { ...deliveryDate }  /></td>
+              <th>Size</th><td><input type="text" { ...size }  /></td>
+            </tr>
+            <tr>
+              <th>Notes</th><td colSpan="3"><textarea { ...description }  /></td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     );
   }
 }
 
+const mapStateToProps = ({ roomSelected }) => { roomSelected };
+
 export default reduxForm({
   form: 'UpdateFurnitureForm',
-  fields: ['itemName'],
-}, null, {})(UpdateFurnitureForm);
+  fields: ['itemName', 'price', 'deliveryDate', 'size', 'description'],
+}, mapStateToProps, { updateFurniture })(UpdateFurnitureForm);
