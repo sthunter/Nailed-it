874e0eee93cdb7d57097ab5876727738fdf3d1b1
Rebero <eber.rene@gmail.com>
2016-08-12 17:19:24 -0700

    refactor: improve efficiency

--- src/designer/drawingtool/components/Mondrian.js
+++ src/designer/drawingtool/components/Mondrian.js
@@ -7,16 +7,61 @@ import { changeRoute } from '../../../routing/actions/routing.action';
 
 export default class Designing extends Component {
   currentRoom = "bathroom"
-
-  componentWillMount() {
-    console.log('store', this.props.store)
+  componentDidUpdate() {
+    this.state = (this.props.rooms[this.currentRoom] && this.props.rooms[this.currentRoom].design)  ?
+      { objects: this.props.rooms[this.props.roomSelected].design } :
+       { objects: [{
+        "width": 300,
+        "height": 245,
+        "rotate": 0,
+        "strokeWidth": 0,
+        "fill": "#424242",
+        "radius": "0",
+        "blendMode": "normal",
+        "type": "rectangle",
+        "x": 10,
+        "y": 5
+      }, {
+        "width": 330,
+        "height": 245,
+        "rotate": 0,
+        "strokeWidth": 0,
+        "fill": "#424242",
+        "radius": "0",
+        "blendMode": "normal",
+        "type": "rectangle",
+        "x": 369,
+        "y": 3
+      }, {
+        "width": 300,
+        "height": 350,
+        "rotate": 0,
+        "strokeWidth": 0,
+        "fill": "rgba(241, 97, 99, 1)",
+        "radius": "0",
+        "blendMode": "normal",
+        "type": "rectangle",
+        "x": 10,
+        "y": 262
+      }, {
+        "width": 143,
+        "height": 160,
+        "rotate": 0,
+        "strokeWidth": 0,
+        "fill": "rgba(255, 241, 0, 1)",
+        "radius": "0",
+        "blendMode": "normal",
+        "type": "rectangle",
+        "x": 190,
+        "y": 16
+      }]
+    }
+    console.log("reset state")
   }
-
   componentDidMount() {
     if(Object.keys(this.props.rooms).length === 0) {
       this.props.getRooms();
     }
-    this.props.changeRoute("/designer")
   }
 
   state = (this.props.rooms[this.currentRoom] && this.props.rooms[this.currentRoom].design)  ?
