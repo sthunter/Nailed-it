bf2712507d506a6ea925c065daa2f8f7db772b10
Ben Patalano <ben.patalano@gmail.com>
2016-08-12 19:15:06 -0700

    refactor(list item): managed to merge through conflict on toolbar

--- src/rooms/components/toolbar.component.jsx
+++ src/rooms/components/toolbar.component.jsx
@@ -6,29 +6,12 @@ import { Row, Col, Modal, Button} from 'react-materialize';
 import { Link, browserHistory } from 'react-router';
 import Dropzone from 'react-dropzone';
 import ColorInput from './colorPicker.component';
-import Designer from '../../designer/drawingtool/App'
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
-
-const customContentStyle = {
-  width: '58%',
-  maxWidth: 'none',
-};
 
 class Toolbar extends Component {
   state = {
     displayColorPicker: false,
-    open: false,
   }
 
-  handleOpen = () => {
-    this.setState({open: true});
-  };
-
-  handleClose = () => {
-    this.setState({open: false});
-  };
 
   handleColorClick () {
     this.setState({ displayColorPicker: !this.state.displayColorPicker })
@@ -41,23 +24,9 @@ class Toolbar extends Component {
     Materialize.toast(title + ' removed', 4000)
   }
   render() {
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        primary={true}
-        onTouchTap={this.handleClose}
-      />,
-      <FlatButton
-        label="Submit"
-        primary={true}
-        keyboardFocused={true}
-        onTouchTap={this.handleClose}
-      />,
-    ];
     const { title } = this.props;
     return (
-      <Row>
-        <Col l={6}>
+      <div>
             <div className='card-control' hoverable>
               <Dropzone style={{'width': '24px', 'height': '24px', 'border': '0px'}} onDrop={files => this.onDrop(files, title)}>
                 <i className="card-controls material-icons md-dark">add_a_photo</i>
@@ -65,20 +34,8 @@ class Toolbar extends Component {
             </div>
             <div className='card-control' hoverable><i className="card-controls material-icons md-dark">create</i></div>
             <div className='card-control' hoverable><i className="card-controls material-icons md-dark" onClick={() => {this.removeRoomCall(title)}}>delete_sweep</i></div>
-            <div className='card-control' hoverable><i className="card-controls material-icons md-dark" onTouchTap={this.handleOpen} >gesture</i></div>
-            <Dialog
-              title="Dialog With Actions"
-              contentStyle={customContentStyle}
-              actions={actions}
-              modal={true}
-              open={this.state.open}
-              onRequestClose={this.handleClose}
-            >
-              <Row><Col s={6}><Designer /></Col></Row>
-            </Dialog>
-            <ColorInput />
-        </Col>
-      </Row>
+            <ColorInput title = { title } />
+      </div>
     )
   }
 }
