4af84bdbe0e7a42025cb0f1bd275a688314a4ee2
Nick Barry <niko.barry@gmail.com>
2016-08-12 17:16:18 -0700

    fix: Fix error thrown when loading a room page directly
    When loading a specific page from its url (e.g., 'http://localhost:3000/furniture/den'), an error would be thrown by ColorPalette. Now that doesn't happen.

--- src/colorPalette/containers/colorPalette.container.jsx
+++ src/colorPalette/containers/colorPalette.container.jsx
@@ -4,21 +4,19 @@ import { connect } from 'react-redux';
 
 class ColorPalette extends Component {
   buildPalette() {
-    let roomSelected = this.props.roomSelected;
-    const color = this.props.rooms[roomSelected].color && this.props.rooms[roomSelected].color.hex;
+    const roomSelectedName = this.props.roomSelected;
+    const room = this.props.rooms[roomSelectedName];
+    const color = room && room.color && room.color.hex;
     let rectWidth = 100;
     return <rect x="0" y="0" fill={color || 'white'} width={rectWidth} height="50" />
   }
 
   render() { 
-    let rects;
-    let rectWidth = 100;
-    rects = this.buildPalette();
     return (
       <div>
-        <svg width={rectWidth * (rects.length - 1) || rectWidth}>
+        <svg width={100}>
           <g>
-          {rects}
+          {this.buildPalette()}
           </g>
         </svg>
       </div>
