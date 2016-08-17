81a14fef1955dd1307bf99af3052016de73ac791
Nick Barry <niko.barry@gmail.com>
2016-08-16 18:59:39 -0700

    feat(frontend): [WIP] Partially refactor furniture table
    The end goal is to allow the user to view a table of furniture, and also click to edit/submit any row. I made some initial progress on this earlier, and it superficially appeared that I was further then than I am now, but this current refactor is meant to solve some problems I was running into in my previous attempt. The new version, when complete, should take care of setting the correct values in the form after a user cancels or saves, and should also allow the user to use the table sort functionality without exiting out of edit mode for all rows.

---
+++ src/furniture/components/listingFurnitureRow.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function listingFurnitureRow(props)  {
+  const _props = props;
+  //console.log('listingFurnitureRow props: ', props);
+  const { roomName, furnitureName, furnitureObj } = props.data;
+
+  function controlsClick(e, type) {
+    // todo: Convert this to a switch-object
+    if (type === 'edit') {
+      e.preventDefault();
+      props.updateEditStatus(furnitureName, true);
+    } else if (type === 'cancel') {
+      e.preventDefault();
+      // todo: reset form to initial values
+      props.updateEditStatus(furnitureName, false);
+    } else if (type === 'submit') {
+      e.preventDefault(); // todo: remove this once I know the data looks right
+      props.updateEditStatus(furnitureName, false);
+    }
+  }
+
+  return props.editing ? (
+    <div>Form will go here</div>
+  ) : (
+    <div className="table-item tr">
+      <span className="td room slimDown"><strong>{ roomName }</strong></span>
+      <span className="td slimDown furniture">{ furnitureName }</span>
+      <span className="td slimDown price">{ furnitureObj.price }</span>
+      <span className="td slimDown size">{ furnitureObj.size }</span>
+      <span className="td slimDown quantity">{ furnitureObj.quantity }</span>
+      <span className="td slimDown notes">{ furnitureObj.notes }</span>
+      <span className="td slimDown deliveryDate">{ furnitureObj.deliveryDate }</span>
+      <span className="td controls slimDown">
+        <button className={ `btn-flat` } onClick={ (e) => controlsClick(e, 'edit') }>Edit</button>
+      </span>
+    </div>
+  );
+}
+
+/*
+ onClick={ (e) => this.controlsClick(e, 'edit') }
+ */
--- src/furniture/containers/updateFurnitureFormTable.container.jsx
+++ src/furniture/containers/updateFurnitureFormTable.container.jsx
@@ -1,56 +1,76 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { updateFurniture } from '../actions/furniture.action';
-import { Card, CardTitle, Row, Col, Input} from 'react-materialize';
 
 class UpdateFurnitureFormTable extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      editing: false,
+    };
+  };
+
+  controlsClick(e, type) {
+    if (type === 'edit') {
+      e.preventDefault();
+      this.setState({ editing: true });
+      console.log('now editing? ', this.state.editing);
+    } else if (type === 'cancel') {
+      e.preventDefault();
+      // todo: reset form to initial values
+      this.setState({editing: false});
+    } else if (type === 'submit') {
+      e.preventDefault(); // todo: remove this once I know the data looks right
+      this.setState({ editing: false });
+    }
+  }
+  
   render() {
     const { roomName, furnitureName, furnitureObj } = this.props.data;
     const { fields: { itemName, price, deliveryDate, size, quantity, description }, handleSubmit } = this.props;
     const boundUpdateFurniture = this.props.updateFurniture.bind(null,
       furnitureName, roomName, furnitureObj
     );
-    let editing = false;
 
     return (
-      <form onSubmit={ handleSubmit(boundUpdateFurniture) }>
-        <tr>
-          <td className="slimDown"><strong>{ roomName }</strong></td>
-          <td className="slimDown">
-            <div className={ editing ? 'hide' : '' }>{ furnitureName }</div>
-            <div className={ editing ? '' : 'hide' }></div>
-          </td>
-          <td className="slimDown">
-            <div className={ editing ? 'hide' : '' }>{  }</div>
-            <input type="text" className={ `${editing ? '' : 'hide' } ${itemName.error ? 'invalid' : 'valid' }` } { ...itemName }  />
-            <div className="edit-view help-text"><span className="form-warning">{ itemName.error }</span></div>
-          </td>
-          <td className="slimDown">
-            <div className={ editing ? 'hide' : '' }>{ furnitureObj.price }</div>
-            <input type="text" className={ `${editing ? '' : 'hide' } ${price.error ? 'invalid' : 'valid' }` } { ...price }  />
-            <div className="edit-view help-text"><span className="form-warning">{ price.error }</span></div>
-          </td>
-          <td className="slimDown">
-            <div className={ editing ? 'hide' : '' }>{ furnitureObj.size }</div>
-            <input type="text" className={ `${editing ? '' : 'hide' } ${size.error ? 'invalid' : 'valid' }` } { ...size }  />
-            <div className="edit-view help-text"><span className="form-warning">{ size.error }</span></div>
-          </td>
-          <td className="slimDown">
-            <div className={ editing ? 'hide' : '' }>{ furnitureObj.quantity }</div>
-            <input type="text" className={ `${editing ? '' : 'hide' } ${quantity.error ? 'invalid' : 'valid' }` } { ...quantity }  />
-            <div className="edit-view help-text"><span className="form-warning">{ quantity.error }</span></div>
-          </td>
-          <td className="slimDown">
-            <div className={ editing ? 'hide' : '' }>{ furnitureObj.notes }</div>
-            <input type="text" className={ `${editing ? '' : 'hide' } ${description.error ? 'invalid' : 'valid' }` } { ...description }  />
-            <div className="edit-view help-text"><span className="form-warning">{ description.error }</span></div>
-          </td>
-          <td className="slimDown">
-            <div className={ editing ? 'hide' : '' }>{ furnitureObj.deliveryDate }</div>
-            <input type="text" className={ `${editing ? '' : 'hide' } ${deliveryDate.error ? 'invalid' : 'valid' }` } { ...deliveryDate }  />
-            <div className="edit-view help-text"><span className="form-warning">{ deliveryDate.error }</span></div>
-          </td>
-        </tr>
+      <form onSubmit={ handleSubmit(boundUpdateFurniture) } className="table-item tr">
+        <span className="td room slimDown"><strong>{ roomName }</strong></span>
+        <span className="td furniture slimDown">
+          <div className={ this.state.editing ? 'hide' : '' }>{ furnitureName }</div>
+          <input type="text" className={ `${this.state.editing ? '' : 'hide' } ${itemName.error ? 'invalid' : 'valid' }` } { ...itemName }  />
+          <div className="edit-view help-text"><span className="form-warning">{ itemName.error }</span></div>
+        </span>
+        <span className="td price slimDown">
+          <div className={ this.state.editing ? 'hide' : '' }>{ furnitureObj.price }</div>
+          <input type="text" className={ `${this.state.editing ? '' : 'hide' } ${price.error ? 'invalid' : 'valid' }` } { ...price }  />
+          <div className="edit-view help-text"><span className="form-warning">{ price.error }</span></div>
+        </span>
+        <span className="td size slimDown">
+          <div className={ this.state.editing ? 'hide' : '' }>{ furnitureObj.size }</div>
+          <input type="text" className={ `${this.state.editing ? '' : 'hide' } ${size.error ? 'invalid' : 'valid' }` } { ...size }  />
+          <div className="edit-view help-text"><span className="form-warning">{ size.error }</span></div>
+        </span>
+        <span className="td quantity slimDown">
+          <div className={ this.state.editing ? 'hide' : '' }>{ furnitureObj.quantity }</div>
+          <input type="text" className={ `${this.state.editing ? '' : 'hide' } ${quantity.error ? 'invalid' : 'valid' }` } { ...quantity }  />
+          <div className="edit-view help-text"><span className="form-warning">{ quantity.error }</span></div>
+        </span>
+        <span className="td notes slimDown">
+          <div className={ this.state.editing ? 'hide' : '' }>{ furnitureObj.notes }</div>
+          <input type="text" className={ `${this.state.editing ? '' : 'hide' } ${description.error ? 'invalid' : 'valid' }` } { ...description }  />
+          <div className="edit-view help-text"><span className="form-warning">{ description.error }</span></div>
+        </span>
+        <span className="td deliveryDate slimDown">
+          <div className={ this.state.editing ? 'hide' : '' }>{ furnitureObj.deliveryDate }</div>
+          <input type="text" className={ `${this.state.editing ? '' : 'hide' } ${deliveryDate.error ? 'invalid' : 'valid' }` } { ...deliveryDate }  />
+          <div className="edit-view help-text"><span className="form-warning">{ deliveryDate.error }</span></div>
+        </span>
+        <span className="td controls slimDown">
+          <button className={ `${this.state.editing ? 'hide' : ''} btn-flat` } onClick={ (e) => this.controlsClick(e, 'edit') }>Edit</button>
+          <button className={ `${this.state.editing ? '' : 'hide'} btn-flat` } onClick={ (e) => this.controlsClick(e, 'submit') } type="submit">Submit</button>
+          <button className={ `${this.state.editing ? '' : 'hide'} btn-flat` } onClick={ (e) => this.controlsClick(e, 'cancel') }>Cancel</button>
+        </span>
       </form>
     );
   }
