ac7bac53223d0572097199db20649dda53b8caff
Ben Patalano <ben.patalano@gmail.com>
2016-08-18 11:23:59 -0700

    refactor: calculate total cost in furniture helper works
    -removed commented out lines in budget helper

--- src/BudgetView/budgetHelperFunctions.js
+++ src/BudgetView/budgetHelperFunctions.js
@@ -53,14 +53,6 @@ const helpers = {
     return arr;
   },
 
-  // getRandomColor() {
-  //   var letters = '0123456789abcdef';
-  //   var color = '#';
-  //   for (var i = 0; i < 6; i++) {
-  //       color += letters[Math.floor(Math.random() * 16)];
-  //   }
-  //   return color;
-  // }
 
 }
 
