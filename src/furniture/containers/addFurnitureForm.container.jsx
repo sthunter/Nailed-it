20fb9f3a39768839cdcc999a4f0cae75f72d070e
Nick Barry <niko.barry@gmail.com>
2016-08-18 15:31:14 -0700

    feat(ui): Add color picker to Add Furniture form
    In addition to being able to type a string for a color of a new piece of furniture, the user can now also use a color picker to auto-fill the field with a hex value.

--- src/furniture/containers/addFurnitureForm.container.jsx
+++ src/furniture/containers/addFurnitureForm.container.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { Card, CardTitle, Icon, Button, Input, Row, Col } from 'react-materialize';
-import { reduxForm, reset } from 'redux-form';
+import { reduxForm, reset, change } from 'redux-form';
 import { addFurniture } from '../actions/furniture.action.js';
 import DatePicker from 'material-ui/DatePicker';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import ColorInput from '../../rooms/components/colorPicker.component.jsx';
 
 class AddFurnitureForm extends Component {
+  handleColorPick(color) {
+
+  }
+
   render() {
     const { fields: {
       itemName, price, description, url, size, color
       }, handleSubmit, } = this.props;
- 
+    //console.log('color: ', color);
+
     return (
       <form onSubmit={ handleSubmit(this.props.addFurniture.bind(null, this.props.roomSelected)) }>
       <Row>
@@ -23,8 +30,9 @@ class AddFurnitureForm extends Component {
         <Input s={6} placeholder='Photo URL'{ ...url } />
       </Row>
       <Row>
-        <Input s={6} placeholder="Size (centimeters)" { ...size } />
-        <Input s={6} placeholder="Primary color" { ...color } />
+        <Input s={4} placeholder="Size (centimeters)" { ...size } />
+        <Input s={4} placeholder="Primary color" { ...color } />
+        <ColorInput s={4} action={ colorObj => this.props.changeFieldValue('color', colorObj.hex) } />
       </Row>
      
       <RaisedButton type="submit" label="submit"/>
@@ -33,10 +41,19 @@ class AddFurnitureForm extends Component {
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    ...bindActionCreators({ addFurniture, change }, dispatch),
+    changeFieldValue: (field, value) => {
+      dispatch(change('AddFurnitureForm', field, value))
+    },
+  };
+}
+
 export default reduxForm({
   form: 'AddFurnitureForm',
   fields: ['itemName', 'price', 'description', 'url', 'size', 'color'],
-}, state => ({ roomSelected: state.roomSelected }), { addFurniture })(AddFurnitureForm);
+}, state => ({ roomSelected: state.roomSelected }), mapDispatchToProps)(AddFurnitureForm);
               
 // {...deliveryDate}
 // <input s={6} type='file' {...photoURL}><i className="material-icons">add_a_photo</i></input>
