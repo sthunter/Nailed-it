da5cc9a9de1278f9bde6b84bb4d5325e6b469827
Nick Barry <niko.barry@gmail.com>
2016-08-20 14:19:43 -0700

    feat(frontend): Add validation to table-view furniture update form
    There are now validation messages if the user deletes a furniture name, or enters non-numeric input for the price.

--- src/furniture/containers/updateFurnitureFormTable.container.jsx
+++ src/furniture/containers/updateFurnitureFormTable.container.jsx
@@ -27,24 +27,26 @@ class UpdateFurnitureFormTable extends Component {
       boundProps, null, null
     );
 
+    console.log('[updateFurnitureFormTable] this.props: ', this.props);
+
     return (
-      <form onSubmit={ handleSubmit(boundUpdateFurniture) } className="table-item tr">
+      <form onSubmit={ handleSubmit(boundUpdateFurniture) } className={ `table-item tr ${this.props.invalid ? 'row-has-error' : ''}` }>
         <span className="td room slimDown"><strong>{ roomName }</strong></span>
         <span className="td furniture slimDown">
-          <input type="text" className={ itemName.error ? 'invalid' : 'valid' } { ...itemName }  />
-          <div className="edit-view help-text"><span className="form-warning">{ itemName.error }</span></div>
+          <div className="edit-view help-text"><span className="form-warning">{ (itemName.error) }</span></div>
+          <input type="text" className={ (itemName.error) ? 'invalid' : 'valid' } { ...itemName }  />
         </span>
         <span className="td price slimDown">
-          <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...price }  />
           <div className="edit-view help-text"><span className="form-warning">{ price.error }</span></div>
+          <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...price }  />
         </span>
         <span className="td size slimDown">
-          <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...size }  />
           <div className="edit-view help-text"><span className="form-warning">{ size.error }</span></div>
+          <input type="text" className={ size.error ? 'invalid' : 'valid' } { ...size }  />
         </span>
         <span className="td color slimDown">
-          <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...color }  />
           <div className="edit-view help-text"><span className="form-warning">{ color.error }</span></div>
+          <input type="text" className={ color.error ? 'invalid' : 'valid' } { ...color }  />
         </span>
         <span className="td controls slimDown">
           <button className="btn-flat" type="submit">Submit</button>
@@ -55,7 +57,22 @@ class UpdateFurnitureFormTable extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.itemName) {
+    errors.itemName = 'The item must have a name.'
+  }
+
+  if (values.price && values.price.match(/\D/)) {
+    errors.price = 'Numbers only.';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'UpdateFurnitureFormTable',
   fields: ['itemName', 'price', 'size', 'color'],
+  validate,
 }, null, { updateFurniture })(UpdateFurnitureFormTable);
