f0a7af87a9a704909ce0f7d3dd9e23b6db9a848e
Sean Hunter <seanhunter@me.com>
2016-08-24 16:58:45 -0700

    style(wip): change table in progress

--- src/furniture/containers/updateFurnitureFormTable.container.jsx
+++ src/furniture/containers/updateFurnitureFormTable.container.jsx
@@ -49,8 +49,64 @@ function validate(values) {
     errors.itemName = 'The item must have a name.'
   }
 
+<<<<<<< cee7153d9f8bc91f165de468c38d11744cada613
   if (values.price && values.price.match(/\D/)) {
     errors.price = 'Numbers only.';
+||||||| merged common ancestors
+    return (
+      <form onSubmit={ handleSubmit(boundUpdateFurniture) } className="table-item tr">
+        <span className="td room slimDown"><strong>{ roomName }</strong></span>
+        <span className="td furniture slimDown">
+          <input type="text" className={ itemName.error ? 'invalid' : 'valid' } { ...itemName }  />
+          <div className="edit-view help-text"><span className="form-warning">{ itemName.error }</span></div>
+        </span>
+        <span className="td price slimDown">
+          <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...price }  />
+          <div className="edit-view help-text"><span className="form-warning">{ price.error }</span></div>
+        </span>
+        <span className="td size slimDown">
+          <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...size }  />
+          <div className="edit-view help-text"><span className="form-warning">{ size.error }</span></div>
+        </span>
+        <span className="td color slimDown">
+          <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...color }  />
+          <div className="edit-view help-text"><span className="form-warning">{ color.error }</span></div>
+        </span>
+        <span className="td controls slimDown">
+          <button className="btn-flat" type="submit">Submit</button>
+          <button className="btn-flat" type="reset" onClick={ (e) => this.props.controlsClick(e, 'cancel') }>Cancel</button>
+        </span>
+      </form>
+    );
+=======
+    return (
+      <tr>
+        <form onSubmit={ handleSubmit(boundUpdateFurniture) } >
+          <td><strong>{ roomName }</strong></td>
+          <td>
+            <input type="text" className={ itemName.error ? 'invalid' : 'valid' } { ...itemName }  />
+            <div className="edit-view help-text"><span className="form-warning">{ itemName.error }</span></div>
+          </td>
+          <td>
+            <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...price }  />
+            <div className="edit-view help-text"><span className="form-warning">{ price.error }</span></div>
+          </td>
+          <td>
+            <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...size }  />
+            <div className="edit-view help-text"><span className="form-warning">{ size.error }</span></div>
+          </td>
+          <td >
+            <input type="text" className={ price.error ? 'invalid' : 'valid' } { ...color }  />
+            <div className="edit-view help-text"><span className="form-warning">{ color.error }</span></div>
+          </td>
+          <td >
+            <button className="btn-flat" type="submit">Submit</button>
+            <button className="btn-flat" type="reset" onClick={ (e) => this.props.controlsClick(e, 'cancel') }>Cancel</button>
+          </td>
+        </form>
+      </tr>
+    );
+>>>>>>> style(wip): change table in progress
   }
 
   return errors;
